0f2e5083cc6a219e101e29c88d7880b39e85820b
JWLK <utxtion@me.com>
2021-04-12 22:35:15 +0900

    Initial Commit

---
+++ ListViewUserManager/ListViewUserManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewUserManagerApp.swift
+//  ListViewUserManager
+//
+//  Created by JWLK on 2021/04/12.
+//
+
+import SwiftUI
+
+@main
+struct ListViewUserManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
